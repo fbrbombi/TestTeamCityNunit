fc735110540fc3e94eac6784c5d41174b82bbcd8
Fabio Bombiela <fabio.bombiela@endava.com>
2020-07-02 17:33:19 -0500

    Test Allure

--- TestNunitTeamCity/Test.cs
+++ TestNunitTeamCity/Test.cs
@@ -1,7 +1,9 @@
-﻿using NUnit.Framework;
+﻿using NUnit.Allure.Core;
+using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
 using System.IO;
 using System.Reflection;
@@ -10,26 +12,44 @@ using System.Threading;
 namespace TestTeamCity
 {
     [TestFixture]
+    [AllureNUnit]
+
     class Test
     {
         ThreadLocal<IWebDriver> driver= new ThreadLocal<IWebDriver>();
-
+        [OneTimeSetUp]
+        public void beforeall()
+        {
+            Environment.CurrentDirectory = Path.GetDirectoryName(GetType().Assembly.Location);
+        }
 
         [SetUp]
         public void startBrowser()
         {
-            driver.Value = new ChromeDriver();
-            driver.Value.Url = "https://www.google.co";
+             ChromeOptions chromeOption = new ChromeOptions();
 
+            chromeOption.AddArgument("headless");
+
+
+            //driver.Value = new ChromeDriver(chromeOption);
+            //driver.Value.Url = "https://www.google.co";
+            //driver.Navigate().GoToUrl($"https://www.google.co");
+
+            //ChromeOption.AddUserProfilePreference("network.proxy.autoconfig_url", "http://pac.zscloud.net/beazley.com/pac01.pac");
+            Thread.Sleep(5000);
         }
 
 
         [Test]
         public void test()
         {
-            Console.WriteLine("AJIACO1");
-            Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
-            Thread.Sleep(15000);
+           
+            //chromeOption.Proxy = proxy;
+
+          
+            //Console.WriteLine(driver.Value.Url);
+
+           
         }
 
         [Test]
@@ -63,14 +83,16 @@ namespace TestTeamCity
         [TearDown]
         public void closeBrowser()
         {
+            Directory.CreateDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/screenshots");
+            //var screenshots = ((ITakesScreenshot)driver.Value).GetScreenshot();
+
+            //screenshots.SaveAsFile(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/screenshots/" + TestContext.CurrentContext.Test.FullName + ".png");
+
             if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
             {
-                Directory.CreateDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+"/screenshots");
-                var screenshots = ((ITakesScreenshot)driver.Value).GetScreenshot();
-                screenshots.SaveAsFile(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/screenshots/" + TestContext.CurrentContext.Test.FullName + ".png");
-            }
-            Console.WriteLine(driver.ToString());
-            driver.Value.Quit();
+               }
+            //Console.WriteLine(driver.ToString());
+            //driver.Value.Quit();
             
         }
     }
