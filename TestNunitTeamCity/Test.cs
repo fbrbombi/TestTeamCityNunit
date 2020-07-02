c50d2f6bd3700d7f70fdbc533595f9806d4fcf4a
Fabio Bombiela <fabio.bombiela@endava.com>
2020-07-02 18:06:53 -0500

    otro test

--- TestNunitTeamCity/Test.cs
+++ TestNunitTeamCity/Test.cs
@@ -23,77 +23,77 @@ namespace TestTeamCity
             Environment.CurrentDirectory = Path.GetDirectoryName(GetType().Assembly.Location);
         }
 
-        [SetUp]
-        public void startBrowser()
-        {
-            ChromeOptions chromeOption = new ChromeOptions();
+        //[SetUp]
+        //public void startBrowser()
+        //{
+        //    ChromeOptions chromeOption = new ChromeOptions();
 
-            chromeOption.AddArgument("headless");
-
-
-            //driver.Value = new ChromeDriver(chromeOption);
-            //driver.Value.Url = "https://www.google.co";
-            //driver.Navigate().GoToUrl($"https://www.google.co");
-
-            //ChromeOption.AddUserProfilePreference("network.proxy.autoconfig_url", "http://pac.zscloud.net/beazley.com/pac01.pac");
-            Thread.Sleep(5000);
-        }
+        //    chromeOption.AddArgument("headless");
 
 
-        [Test]
-        public void test()
-        {
+        //    //driver.Value = new ChromeDriver(chromeOption);
+        //    //driver.Value.Url = "https://www.google.co";
+        //    //driver.Navigate().GoToUrl($"https://www.google.co");
 
-            //chromeOption.Proxy = proxy;
+        //    //ChromeOption.AddUserProfilePreference("network.proxy.autoconfig_url", "http://pac.zscloud.net/beazley.com/pac01.pac");
+        //    Thread.Sleep(5000);
+        //}
 
 
-            //Console.WriteLine(driver.Value.Url);
+        //[Test]
+        //public void test()
+        //{
+
+        //    //chromeOption.Proxy = proxy;
 
 
-        }
+        //    //Console.WriteLine(driver.Value.Url);
 
-        [Test]
-        public void test2()
-        {
-            Console.WriteLine("AJIACO2");
-            Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
-            Thread.Sleep(15000);
-            Assert.IsTrue(false);
 
-        }
-        [Test]
-        public void test3()
-        {
-            Console.WriteLine("AJIACO3");
-            Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
-            Thread.Sleep(15000);
-            Assert.IsTrue(false);
+        //}
 
-        }
+        //[Test]
+        //public void test2()
+        //{
+        //    Console.WriteLine("AJIACO2");
+        //    Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
+        //    Thread.Sleep(15000);
+        //    Assert.IsTrue(false);
 
-        [Test]
-        public void test4()
-        {
-            Console.WriteLine("AJIACO4");
-            Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
-            Thread.Sleep(15000);
+        //}
+        //[Test]
+        //public void test3()
+        //{
+        //    Console.WriteLine("AJIACO3");
+        //    Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
+        //    Thread.Sleep(15000);
+        //    Assert.IsTrue(false);
 
-        }
+        //}
 
-        [TearDown]
-        public void closeBrowser()
-        {
-            Directory.CreateDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/screenshots");
-            //var screenshots = ((ITakesScreenshot)driver.Value).GetScreenshot();
+        //[Test]
+        //public void test4()
+        //{
+        //    Console.WriteLine("AJIACO4");
+        //    Console.WriteLine(Environment.GetEnvironmentVariable("Ambiente"));
+        //    Thread.Sleep(15000);
 
-            //screenshots.SaveAsFile(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/screenshots/" + TestContext.CurrentContext.Test.FullName + ".png");
+        //}
 
-            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
-            {
-            }
-            //Console.WriteLine(driver.ToString());
-            //driver.Value.Quit();
+        //[TearDown]
+        //public void closeBrowser()
+        //{
+        //    Directory.CreateDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/screenshots");
+        //    //var screenshots = ((ITakesScreenshot)driver.Value).GetScreenshot();
 
-        }
+        //    //screenshots.SaveAsFile(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/screenshots/" + TestContext.CurrentContext.Test.FullName + ".png");
+
+        //    if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+        //    {
+        //    }
+        //    //Console.WriteLine(driver.ToString());
+        //    //driver.Value.Quit();
+
+        //}
     }
 }
